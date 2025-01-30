8d794eb2708902db95f93c2324fd45b83022cce2
asserben <asser.benabdallah@gmail.com>
2025-01-30 15:02:55 +0100

    fix pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,50 +1,28 @@
 #!/usr/bin/python3
-"""
-This module provides a function to determine if all lockboxes can be unlocked.
-
-The function `can_unlock_all` checks if all boxes in a given list can be unlocked
-using the keys available in the boxes.
-"""
+"""LOCKBOXES"""
 
 
-def can_unlock_all(boxes):
+def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked.
 
     Args:
-        boxes (list of list of int): A list of boxes, where each box contains
-                                    a list of keys (integers).
+        boxes (list of list of int): A list of lists where each sublist contains keys.
 
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
-    
-
-
-
-
-
-
-
-
-
-# #!/usr/bin/python3
-# #Solve IT
-
-# def canUnlockAll(boxes):
-#     """Solve"""
-#     #PUT THE KEYS
-#     keys=[]
-#     keys.extend(boxes[0])
-#     i=1
-#     while(i<len(boxes)):
-#         if(i not in keys):
-#             break
-#         else:
-#             keys.extend(boxes[i])
-#             i+=1
-#     if (i in keys):
-#         return True
-#     else:
-#         return False
-    
+    # PUT THE KEYS
+    keys = []
+    keys.extend(boxes[0])
+    i = 1
+    while i < len(boxes):
+        if i not in keys:
+            break
+        else:
+            keys.extend(boxes[i])
+            i += 1
+    if i in keys:
+        return True
+    else:
+        return False
