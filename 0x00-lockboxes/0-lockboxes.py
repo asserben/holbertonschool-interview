f953991c50cd3b6570ca0ea4c4288b85ef53fa2c
asserben <asser.benabdallah@gmail.com>
2025-01-30 10:34:48 +0100

    test 7

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3
-#Solve IT
+"""
+ Unlocked the boxes
+"""
+
+
 def canUnlockAll(boxes):
-    """Solve"""
-    #PUT THE KEYS
-    keys=[]
-    keys.extend(boxes[0])
-    i=1
-    while(i<len(boxes)):
-        if(i not in keys):
-            break
-        else:
-            keys.extend(boxes[i])
-            i+=1
-    if (i in keys):
-        return True
-    else:
-        return False
+    """
+        Look if the box is unlocked or not
+        Return true if is unlocked or false
+    """
+
+
+
+
+
+
+
+
+
+# #!/usr/bin/python3
+# #Solve IT
+
+# def canUnlockAll(boxes):
+#     """Solve"""
+#     #PUT THE KEYS
+#     keys=[]
+#     keys.extend(boxes[0])
+#     i=1
+#     while(i<len(boxes)):
+#         if(i not in keys):
+#             break
+#         else:
+#             keys.extend(boxes[i])
+#             i+=1
+#     if (i in keys):
+#         return True
+#     else:
+#         return False
     
