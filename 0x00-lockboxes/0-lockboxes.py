e707e3bd93b599b51f2a03c0273996671aa98e47
asserben <asser.benabdallah@gmail.com>
2025-01-30 15:27:24 +0100

    fix 2

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
 """
-Module for checking if all lockboxes can be unlocked.
-
-This module provides the `canUnlockAll` function, which determines whether all
-lockboxes in a given list can be unlocked using the keys inside them. H
+Unlocked Boxes
 """
 
 
+def join(T, R):
+    """"""
+    res = []
+    for e in R:
+        res = T[e]
+    return res
+
+
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be unlocked.
+    """"""
+    index = 0
+    total = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes, total[index:]):
+            if j not in total:
+                total.append(j)
+                index = 1
+                added = True
 
-    Args:
-        boxes (list of list of int): A list of lists where each sublist contains keys.
-
-    Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
-    """
-    # PUT THE KEYS
-    keys = []
-    keys.extend(boxes[0])
-    i = 1
-    while i < len(boxes):
-        if i not in keys:
-            break
-        else:
-            keys.extend(boxes[i])
-            i += 1
-    if i in keys:
-        return True
-    else:
-        return False
+    return len(total) == len(boxes)
